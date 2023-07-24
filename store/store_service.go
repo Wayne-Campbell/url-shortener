dbbedf895e2949d3bfd2e0813b70963d0f70cb9d
Wayne-Campbell <97609437+Wayne-Campbell@users.noreply.github.com>
2023-07-24 13:21:53 -0400

    Initializing the store service(redis client)

--- store/store_service.go
+++ store/store_service.go
@@ -19,3 +19,21 @@ var(
 )
 
 const CacheDuration = 6 * time.Hour
+
+//initializing store service and returning a store pointer
+func InitializeStore() *StorageService {
+	redisClient := redis.NewClient(&redis.Options{
+		Addr: "localhost:6379",
+		Password: "",
+		DB:		0,
+	})
+pong, err := redisClient.Ping(ctx).Result()
+if err != nil {
+	panic(fmt.Sprintf("Error init Redis: %v", err))
+}
+
+fmt.Printf("\nRedis started succesfully: pong message = {%s}", pong)
+storeService.redisClient = redisClient
+return storeService
+
+}
