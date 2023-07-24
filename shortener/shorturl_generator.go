394a8be8120bff735ff47cf841e1159418d0c65b
Wayne-Campbell <97609437+Wayne-Campbell@users.noreply.github.com>
2023-07-24 16:32:29 -0400

    Added SHA256 and base58 functions

--- shortener/shorturl_generator.go
+++ shortener/shorturl_generator.go
@@ -2,10 +2,23 @@ package shortener
 
 import (
 	"crypto/sha256"
+	"fmt"
+	"github.com/itchy/base58-go"
+	"os"
 )
 
 func sha2560f(input string) []byte {
 	algorithm := sha256.New()
 	algorithm.Write([]byte(input))
 	return algorithm.Sum(nil)
+}
+
+func base58Encoded(bytes []byte) string {
+	encoding := base58.BitcoinEncoding
+	encoded, err := encoding.Encode(bytes)
+	if err != nil {
+		fmt.Println(err.Error())
+		os.Exit(1)
+	}
+	return string(encoded)
 }
