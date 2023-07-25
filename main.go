3ee708811155b31ef92ef6facbfe46fc45ce22bd
Wayne-Campbell <97609437+Wayne-Campbell@users.noreply.github.com>
2023-07-25 13:03:48 -0400

    implement handlers into main.go

--- main.go
+++ main.go
@@ -2,16 +2,29 @@ package main
 
 import (
 	"fmt"
+	"github.com/wayne-campbell/url-shortener/handler"
+	"github.com/wayne-campbell/url-shortener/store"
 	"github.com/gin-gonic/gin"
+	
 )
 
 func main(){
 	r := gin.Default()
 	r.GET("/", func(c *gin.Context){
 		c.JSON(200, gin.H{
-			"message": "Hey Go URL Shortener !",
+			"message": "Welcome to URL shortener API!",
 		})
 	})
+//API post
+r.POST("/create-short-url", func(c *gin.Context){
+	handler.CreateShortUrl(c)
+})
+//API GET
+r.GET("/:shortUrl", func(c *gin.Context){
+	handler.HandleShortUrlRedirect(c)
+})
+//initialize store
+store.InitializeStore()
 
 err := r.Run(":9808")
 if err != nil {
