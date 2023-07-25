c2032005575ced3ffa3f5a552f402000aaf6868e
Wayne-Campbell <97609437+Wayne-Campbell@users.noreply.github.com>
2023-07-25 13:25:21 -0400

    Implement handlers

--- handler/handlers.go
+++ handler/handlers.go
@@ -1,13 +1,39 @@
 package handler
 
 import(
+	"github.com/wayne-campbell/url-shortener/shortener"
+	"github.com/wayne-campbell/url-shortener/store"
 	"github.com/gin-gonic/gin"
+	"net/http"
 )
 
-func CreateShortUrl(c *gin.Context){
+//request model definition
+type UrlCreationRequest struct{
+	LongUrl string 'json:"long_url" binding:"required'
+	UserId string 'json:"user_id" binding:"required'
+}
 
+func CreateShortUrl(c *gin.Context){
+	var creationRequest UrlCreationRequest
+	if err:= c.ShouldBindJSON(&creationRequest); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+	//generates shortened hash
+	shortUrl := shortener.GenerateShortLink(creationRequest.LongUrl, creationRequest.UserId)
+	store.SaveUrlMapping(shortUrl, creationRequest.LongUrl, creationRequest.UserId)
+
+	host := "http:localhost:9808/"
+	c.JSON(200, gin.H{
+		"message": "short url created successfully"
+		"short_url": host +shortUrl,
+	})
 }
 
 func HandleShortUrlRedirect(c *gin.Context){
-
+	shortUrl := c.Param("shortUrl")
+	//retrieve initial url from the provided short url
+	initialUrl := store.RetrieveInitialUrl(shortUrl)
+	//apply http redirection functions
+	c.Redirect(302, initialUrl)
 }
